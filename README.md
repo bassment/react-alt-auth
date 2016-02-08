5ace73dcb278582e93e6fdab96a5a801b921f499
bassment <anton.perebyinis@gmail.com>
2016-02-08 12:35:53 +0200

    Change var to const on serverfiles. Add eslint rules

--- README.md
+++ README.md
@@ -1,13 +1,10 @@
 # react-alt-auth
 _Template project for creating React applications using Alt(login and signUp logic included)_ using:
 
-**Node, Express, React, Alt, ES6, ES7, React-Router** 
+**Node, Express, React, Alt, ES6, ES7, React-Router**
 
 **Webpack, HotModuleReplacement, Firebase, React-Login, Google oAuth**
 
-**Skeleton, Css Modules, Deploy (webpack production version)** 
+**Skeleton, Css Modules, Deploy (webpack production version)**
 
 Full-stack application.
-
-
-[Demo](http://pixelant.space/) project using this template: http://pixelant.space/
--- server/routes/accounts.js
+++ server/routes/accounts.js
@@ -1,14 +1,17 @@
-var Firebase = require('firebase');
-var crypto = require('crypto');
+/* eslint strict: 0, no-console: 0 */
+'use strict';
 
-var firebase = new Firebase('https://automat.firebaseio.com/');
-var users = firebase.child('users');
+const Firebase = require('firebase');
+const crypto = require('crypto');
+
+const firebase = new Firebase('https://automat.firebaseio.com/');
+const users = firebase.child('users');
 
 function hash(password) {
   return crypto.createHash('sha512').update(password).digest('hex');
 }
 
-var router = require('express').Router();
+const router = require('express').Router();
 
 router.use(require('body-parser').json());
 router.use(require('cookie-parser')());
@@ -17,19 +20,20 @@ router.use(require('express-session')({
   saveUninitialized: true,
   secret: '1234qwerty'
 }));
-router.post('/api/signup', function(req, res) {
-  var username = req.body.username;
-  var password = req.body.password;
+router.post('/api/signup', function cb(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
 
-  if (!username || !password)
+  if (!username || !password) {
     return res.json({signedIn: false, message: 'No username or password'});
+  }
 
-  users.child(username).once('value', function(snapshot) {
+  users.child(username).once('value', function get(snapshot) {
     if (snapshot.exists()) {
       return res.json({signedIn: false, message: 'User already exists'});
     }
 
-    var userObj = {
+    const userObj = {
       username: username,
       passwordHash: hash(password)
     };
@@ -41,25 +45,26 @@ router.post('/api/signup', function(req, res) {
   });
 });
 
-router.post('/api/signin', function(req, res) {
-  var username = req.body.username;
-  var password = req.body.password;
+router.post('/api/signin', function cb(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
 
-  if (!username || !password)
+  if (!username || !password) {
     return res.json({signedIn: false, message: 'No username or password'});
+  }
 
-  users.child(username).once('value', function(snapshot) {
+  users.child(username).once('value', function get(snapshot) {
     if (!snapshot.exists() || snapshot.child('passwordHash').val() !== hash(password)) {
       return res.json({signedIn: false, message: 'Wrong username or password'});
     }
 
-    var user = snapshot.exportVal();
+    const user = snapshot.exportVal();
     req.session.user = user;
     res.json({signedIn: true, user: user});
   });
 });
 
-router.post('/api/signout', function(req, res) {
+router.post('/api/signout', function cb(req, res) {
   delete req.session.user;
   res.json({signedIn: false, message: 'You signed out successfully!'});
 });
